c552339d910c0a43d35048a8676ade20493d5357
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-04-12 13:59:45 -0400

    feat: adapt config file to ella
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/gnb/gnb.go
+++ internal/gnb/gnb.go
@@ -1,21 +1,16 @@
 package gnb
 
 import (
-	"os"
-	"os/signal"
-	"sync"
-	"time"
+	"fmt"
 
 	"github.com/ellanetworks/core-tester/internal/config"
 	"github.com/ellanetworks/core-tester/internal/gnb/context"
 	serviceNas "github.com/ellanetworks/core-tester/internal/gnb/nas/service"
 	"github.com/ellanetworks/core-tester/internal/gnb/ngap"
 	"github.com/ellanetworks/core-tester/internal/gnb/ngap/trigger"
-	"github.com/ellanetworks/core-tester/internal/monitoring"
-	log "github.com/sirupsen/logrus"
 )
 
-func InitGnb(conf config.Config, wg *sync.WaitGroup) *context.GNBContext {
+func InitGnb(conf config.Config) (*context.GNBContext, error) {
 	// instance new gnb.
 	gnb := &context.GNBContext{}
 
@@ -27,130 +22,25 @@ func InitGnb(conf config.Config, wg *sync.WaitGroup) *context.GNBContext {
 		conf.GNodeB.PlmnList.Tac,
 		conf.GNodeB.SliceSupportList.Sst,
 		conf.GNodeB.SliceSupportList.Sd,
-		conf.GNodeB.ControlIF.AddrPort,
-		conf.GNodeB.DataIF.AddrPort,
+		conf.GNodeB.N2.AddrPort,
+		conf.GNodeB.N3.AddrPort,
 	)
 
-	// start communication with AMF (server SCTP).
-	for _, amfConfig := range conf.AMFs {
-		// new AMF context.
-		amf := gnb.NewGnBAmf(amfConfig.AddrPort)
+	// start communication with Ella (server SCTP).
+	// new Ella context.
+	ella := conf.Ella
+	amf := gnb.NewGnBAmf(ella.N2.AddrPort)
 
-		// start communication with AMF(SCTP).
-		if err := ngap.InitConn(amf, gnb); err != nil {
-			log.Fatal("Error in", err)
-		} else {
-			log.Info("[GNB] SCTP/NGAP service is running")
-			// wg.Add(1)
-		}
-
-		trigger.SendNgSetupRequest(gnb, amf)
+	// start communication with Ella(SCTP).
+	err := ngap.InitConn(amf, gnb)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to SCTP: %w", err)
 	}
+
+	trigger.SendNgSetupRequest(gnb, amf)
 
 	// start communication with UE (server UNIX sockets).
 	serviceNas.InitServer(gnb)
 
-	go func() {
-		// control the signals
-		sigGnb := make(chan os.Signal, 1)
-		signal.Notify(sigGnb, os.Interrupt)
-
-		// Block until a signal is received.
-		<-sigGnb
-		// gnb.Terminate()
-		wg.Done()
-	}()
-
-	return gnb
-}
-
-func InitGnbForLoadSeconds(conf config.Config, wg *sync.WaitGroup,
-	monitor *monitoring.Monitor,
-) {
-	// instance new gnb.
-	gnb := &context.GNBContext{}
-
-	// new gnb context.
-	gnb.NewRanGnbContext(
-		conf.GNodeB.PlmnList.GnbId,
-		conf.GNodeB.PlmnList.Mcc,
-		conf.GNodeB.PlmnList.Mnc,
-		conf.GNodeB.PlmnList.Tac,
-		conf.GNodeB.SliceSupportList.Sst,
-		conf.GNodeB.SliceSupportList.Sd,
-		conf.GNodeB.ControlIF.AddrPort,
-		conf.GNodeB.DataIF.AddrPort)
-
-	// start communication with AMF (server SCTP).
-	for _, amf := range conf.AMFs {
-		// new AMF context.
-		amf := gnb.NewGnBAmf(amf.AddrPort)
-
-		// start communication with AMF(SCTP).
-		err := ngap.InitConn(amf, gnb)
-		if err != nil {
-			log.Errorf("could not connect to AMF: %v", err)
-		}
-
-		trigger.SendNgSetupRequest(gnb, amf)
-
-		// timeout is 1 second for receive NG Setup Response
-		time.Sleep(1000 * time.Millisecond)
-
-		// AMF responds message sends by Tester
-		// means AMF is available
-		if amf.GetState() == 0x01 {
-			monitor.IncRqs()
-		}
-	}
-
-	gnb.Terminate()
-	wg.Done()
-	// os.Exit(0)
-}
-
-func InitGnbForAvaibility(conf config.Config,
-	monitor *monitoring.Monitor,
-) {
-	// instance new gnb.
-	gnb := &context.GNBContext{}
-
-	// new gnb context.
-	gnb.NewRanGnbContext(
-		conf.GNodeB.PlmnList.GnbId,
-		conf.GNodeB.PlmnList.Mcc,
-		conf.GNodeB.PlmnList.Mnc,
-		conf.GNodeB.PlmnList.Tac,
-		conf.GNodeB.SliceSupportList.Sst,
-		conf.GNodeB.SliceSupportList.Sd,
-		conf.GNodeB.ControlIF.AddrPort,
-		conf.GNodeB.DataIF.AddrPort)
-
-	// start communication with AMF (server SCTP).
-	for _, amf := range conf.AMFs {
-		// new AMF context.
-		amf := gnb.NewGnBAmf(amf.AddrPort)
-
-		// start communication with AMF(SCTP).
-		if err := ngap.InitConn(amf, gnb); err != nil {
-			log.Info("Error in ", err)
-			return
-		} else {
-			log.Info("[GNB] SCTP/NGAP service is running")
-		}
-
-		trigger.SendNgSetupRequest(gnb, amf)
-
-		// timeout is 1 second for receive NG Setup Response
-		time.Sleep(1000 * time.Millisecond)
-
-		// AMF responds message sends by Tester
-		// means AMF is available
-		if amf.GetState() == 0x01 {
-			monitor.IncAvaibility()
-		}
-	}
-
-	gnb.Terminate()
-	// os.Exit(0)
+	return gnb, nil
 }
