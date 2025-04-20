00c6f542ab6e1af85bc1fd8fd2d748f0586ac147
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-04-20 08:40:28 -0400

    fix: attach ebpf program to ingress of veth0
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/ue/gtp/xdp.go
+++ internal/ue/gtp/xdp.go
@@ -55,7 +55,7 @@ func AttachEbpfProgram(opts *AttachEbpfProgramOptions) error {
 	tcLink, err := link.AttachTCX(link.TCXOptions{
 		Interface: iface.Index,
 		Program:   objs.UpstreamProgFunc,
-		Attach:    ebpf.AttachTCXEgress,
+		Attach:    ebpf.AttachTCXIngress,
 	})
 	if err != nil {
 		return fmt.Errorf("could not attach TC program: %w", err)
