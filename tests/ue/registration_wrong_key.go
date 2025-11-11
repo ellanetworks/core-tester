c5248aaa5711960d2a47a3791124260806c374a7
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-11-11 08:50:41 -0500

    feat: add authentication response test with wrong keys (#52)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

---
+++ tests/ue/registration_wrong_key.go
@@ -0,0 +1,90 @@
+package ue
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ellanetworks/core-tester/internal/engine"
+	"github.com/ellanetworks/core-tester/internal/gnb"
+	"github.com/ellanetworks/core-tester/internal/ue"
+	"github.com/ellanetworks/core-tester/internal/ue/sidf"
+	"github.com/ellanetworks/core-tester/tests/utils"
+	"github.com/ellanetworks/core-tester/tests/utils/procedure"
+)
+
+type AuthenticationWrongKey struct{}
+
+func (AuthenticationWrongKey) Meta() engine.Meta {
+	return engine.Meta{
+		ID:      "ue/authentication/wrong_key",
+		Summary: "UE authentication failure test validating the Authentication Request and Response procedures",
+		Timeout: 1 * time.Second,
+	}
+}
+
+func (t AuthenticationWrongKey) Run(ctx context.Context, env engine.Env) error {
+	gNodeB, err := gnb.Start(env.CoreConfig.N2Address, env.GnbN2Address)
+	if err != nil {
+		return fmt.Errorf("error starting gNB: %v", err)
+	}
+
+	defer gNodeB.Close()
+
+	err = procedure.NGSetup(ctx, &procedure.NGSetupOpts{
+		Mcc:    env.CoreConfig.MCC,
+		Mnc:    env.CoreConfig.MNC,
+		Sst:    env.CoreConfig.SST,
+		Tac:    env.CoreConfig.TAC,
+		GnodeB: gNodeB,
+	})
+	if err != nil {
+		return fmt.Errorf("NGSetupProcedure failed: %v", err)
+	}
+
+	newUE, err := ue.NewUE(&ue.UEOpts{
+		Msin: "2989077253",
+		K:    "369f7bd3067faec142c47ed9132e942a",
+		OpC:  "34e89843fe0683dc961873ebc05b8a35",
+		Amf:  "80000000000000000000000000000000",
+		Sqn:  "000000000001",
+		Mcc:  env.CoreConfig.MCC,
+		Mnc:  env.CoreConfig.MNC,
+		HomeNetworkPublicKey: sidf.HomeNetworkPublicKey{
+			ProtectionScheme: "0",
+			PublicKeyID:      "0",
+		},
+		RoutingIndicator: "0000",
+		DNN:              env.CoreConfig.DNN,
+		Sst:              env.CoreConfig.SST,
+		Sd:               env.CoreConfig.SD,
+		IMEISV:           "3569380356438091",
+		UeSecurityCapability: utils.GetUESecurityCapability(&utils.UeSecurityCapability{
+			Integrity: utils.IntegrityAlgorithms{
+				Nia2: true,
+			},
+			Ciphering: utils.CipheringAlgorithms{
+				Nea0: true,
+				Nea2: true,
+			},
+		}),
+	})
+	if err != nil {
+		return fmt.Errorf("could not create UE: %v", err)
+	}
+
+	err = procedure.AuthenticationResponseWrongKeys(ctx, &procedure.AuthenticationResponseWrongKeysOpts{
+		Mcc:         env.CoreConfig.MCC,
+		Mnc:         env.CoreConfig.MNC,
+		Tac:         env.CoreConfig.TAC,
+		GNBID:       GNBID,
+		RANUENGAPID: RANUENGAPID,
+		UE:          newUE,
+		GnodeB:      gNodeB,
+	})
+	if err != nil {
+		return fmt.Errorf("initial registration procedure failed: %v", err)
+	}
+
+	return nil
+}
---
+++ tests/utils/procedure/initial_registration_wrong_key.go
@@ -0,0 +1,123 @@
+package procedure
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ellanetworks/core-tester/internal/gnb"
+	"github.com/ellanetworks/core-tester/internal/ue"
+	"github.com/ellanetworks/core-tester/tests/utils"
+	"github.com/ellanetworks/core-tester/tests/utils/validate"
+	"github.com/free5gc/nas/nasMessage"
+	"github.com/free5gc/ngap/ngapType"
+)
+
+type AuthenticationResponseWrongKeysOpts struct {
+	Mcc         string
+	Mnc         string
+	Tac         string
+	GNBID       string
+	RANUENGAPID int64
+	UE          *ue.UE
+	GnodeB      *gnb.GnodeB
+}
+
+func AuthenticationResponseWrongKeys(ctx context.Context, opts *AuthenticationResponseWrongKeysOpts) error {
+	nasPDU, err := ue.BuildRegistrationRequest(&ue.RegistrationRequestOpts{
+		RegistrationType:  nasMessage.RegistrationType5GSInitialRegistration,
+		RequestedNSSAI:    nil,
+		UplinkDataStatus:  nil,
+		IncludeCapability: false,
+		UESecurity:        opts.UE.UeSecurity,
+	})
+	if err != nil {
+		return fmt.Errorf("could not build Registration Request NAS PDU: %v", err)
+	}
+
+	err = opts.GnodeB.SendInitialUEMessage(&gnb.InitialUEMessageOpts{
+		Mcc:                   opts.Mcc,
+		Mnc:                   opts.Mnc,
+		GnbID:                 opts.GNBID,
+		Tac:                   opts.Tac,
+		RanUENGAPID:           opts.RANUENGAPID,
+		NasPDU:                nasPDU,
+		Guti5g:                opts.UE.UeSecurity.Guti,
+		RRCEstablishmentCause: ngapType.RRCEstablishmentCausePresentMoSignalling,
+	})
+	if err != nil {
+		return fmt.Errorf("could not send InitialUEMessage: %v", err)
+	}
+
+	fr, err := opts.GnodeB.ReceiveFrame(ctx)
+	if err != nil {
+		return fmt.Errorf("could not receive SCTP frame: %v", err)
+	}
+
+	downlinkNASTransport, err := validate.DownlinkNASTransport(&validate.DownlinkNASTransportOpts{
+		Frame: fr,
+	})
+	if err != nil {
+		return fmt.Errorf("DownlinkNASTransport validation failed: %v", err)
+	}
+
+	amfUENGAPID := utils.GetAMFUENGAPIDFromDownlinkNASTransport(downlinkNASTransport)
+	if amfUENGAPID == nil {
+		return fmt.Errorf("could not get AMF UE NGAP ID from DownlinkNASTransport: %v", err)
+	}
+
+	_, _, err = validate.AuthenticationRequest(&validate.AuthenticationRequestOpts{
+		NASPDU: utils.GetNASPDUFromDownlinkNasTransport(downlinkNASTransport),
+		UE:     opts.UE,
+	})
+	if err != nil {
+		return fmt.Errorf("NAS PDU validation failed: %v", err)
+	}
+
+	paramAutn := []byte{
+		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+		0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+	} // Wrong key for test
+
+	authResp, err := ue.BuildAuthenticationResponse(&ue.AuthenticationResponseOpts{
+		AuthenticationResponseParam: paramAutn,
+		EapMsg:                      "",
+	})
+	if err != nil {
+		return fmt.Errorf("could not build authentication response: %v", err)
+	}
+
+	err = opts.GnodeB.SendUplinkNASTransport(&gnb.UplinkNasTransportOpts{
+		AMFUeNgapID: amfUENGAPID.Value,
+		RANUeNgapID: opts.RANUENGAPID,
+		Mcc:         opts.Mcc,
+		Mnc:         opts.Mnc,
+		GnbID:       opts.GNBID,
+		Tac:         opts.Tac,
+		NasPDU:      authResp,
+	})
+	if err != nil {
+		return fmt.Errorf("could not send UplinkNASTransport: %v", err)
+	}
+
+	fr, err = opts.GnodeB.ReceiveFrame(ctx)
+	if err != nil {
+		return fmt.Errorf("could not receive SCTP frame: %v", err)
+	}
+
+	downlinkNASTransport, err = validate.DownlinkNASTransport(&validate.DownlinkNASTransportOpts{
+		Frame: fr,
+	})
+	if err != nil {
+		return fmt.Errorf("DownlinkNASTransport validation failed: %v", err)
+	}
+
+	err = validate.AuthenticationReject(&validate.AuthenticationRejectOpts{
+		NASPDU: utils.GetNASPDUFromDownlinkNasTransport(downlinkNASTransport),
+		UE:     opts.UE,
+	})
+	if err != nil {
+		return fmt.Errorf("could not validate Authentication Reject: %v", err)
+	}
+
+	return nil
+}
